d476868d8a1caae0829713d476eb38e19d2bfb92
annagzowska <a.gzowska@gmail.com>
2016-09-13 13:55:41 +0200

    Update everySecLetter.js

--- everySecLetter.js
+++ everySecLetter.js
@@ -1,3 +1,5 @@
+// function, which returns the word combined from every second letter of the word given
+
 function everySecLetter(text) {
   var result = "";
   for (i = 0; i < text.length; i = i + 2) {
