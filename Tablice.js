de27d838de59a61da933e02afbe4462b330d3be2
annagzowska <a.gzowska@gmail.com>
2016-09-13 14:14:03 +0200

    Update Tablice.js

--- Tablice.js
+++ Tablice.js
@@ -1,37 +1,37 @@
-// function, which calculate sum, avarage and counts the elements of the table
+// function, which calculate sum, avarage and counts the elements of an array
 
 var wynik = 0;
 var tablica1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 15];
 
-function calculateTable(a) {
+function calculateArr(a) {
 
   for (i = 0; i < tablica1.length; i++) {
     wynik += tablica1[i];
   }
-  console.log("Sum of the table is " + wynik)
-  console.log("There are " + a.length + "elements in a table")
-  console.log("Average of the list is " + wynik / tablica1.length)
+  console.log("Sum of the array is " + wynik)
+  console.log("There are " + a.length + "elements in the array")
+  console.log("Average of the array is " + wynik / tablica1.length)
 
 }
 
-calculateTable(tablica1);
+calculateArr(tablica1);
 
 
-// function, which search for a word given in a table
+// function, which search for a word given in an array
 
-var table1 = ["cos", "siano", "stodola", "igla", "kubek", "biurko"];
+var array1 = ["cos", "siano", "stodola", "igla", "kubek", "biurko"];
 
-function findWord(tab1, word1) {
-  for (i = 1; i <= tab1.length; i++) {
+function findWord(arr1, word1) {
+  for (i = 1; i <= arr1.length; i++) {
 
-    if (word1 === tab1[i - 1]) {
+    if (word1 === arr1[i - 1]) {
 
       return "Word " + word1 + " found on position " + i;
     }
 
   }
-  return "I couldn't find word " + word1 + " in a table"
+  return "I couldn't find word " + word1 + " in the array"
 }
 
-findWord(table1, "igla");
+findWord(array1, "igla");
 
