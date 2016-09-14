51559553d7575378cd009db499120682a3a73b0d
annagzowska <a.gzowska@gmail.com>
2016-09-14 17:32:46 +0200

    Update palindrom.js

--- palindrom.js
+++ palindrom.js
@@ -1,23 +1,15 @@
 function palindrom(slowo) {
-
-  let dlugosc = slowo.length;
-  var sprawdz = 0;
+  let dlugosc = slowo.length,
+      sprawdz = 0;
   for (i = 0; i <= slowo.length; i++) {
-
-
     if (slowo[dlugosc - i - 1] === slowo[i]) {
-
       sprawdz += 1;
     }
-
   }
-
   if (sprawdz >= slowo.length) {
     console.log("Slowo " + slowo + " jest palindromem. Wygrales karton ciastek!")
   } else {
     console.log("Slowo " + slowo + " nie jest palindromem. Musisz sam kupic ciastka :)")
   }
-
 }
-
 palindrom("kajak");
